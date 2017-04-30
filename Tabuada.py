8c4b07fe94405b00505e6341974d127b09608e86
atilarangel <rangelaraujoatila@gmail.com>
2017-04-30 01:31:57 -0300

    Add files via upload

---
+++ Tabuada.py
@@ -0,0 +1,10 @@
+#Atividade feita com o CodeAcademy www.codecademy.com
+linha = 1
+coluna = 1
+while linha <= 10:
+    while coluna <=10:
+        print(linha*coluna, end = "\t")
+        coluna = coluna + 1
+    linha = linha + 1
+    print()
+    coluna = 1
